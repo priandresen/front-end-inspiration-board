148da878657054863fba6ac9621ba299e18e6ee2
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2026-01-08 03:14:27 -0800

    feat: add floating action buttons for adding and removing cards; refactor CardList component

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import Card from './Card.jsx';
 import './CardList.css';
 
-const CardList = ({ cards, onDeleteCard, onLikeCard, onDeleteCardsInBoard, boardId }) => {
+const CardList = ({ cards, onDeleteCard, onLikeCard }) => {
   const getCardListJSX = (cardList) => {
     return cardList.map((card) => {
       return (
@@ -17,26 +17,10 @@ const CardList = ({ cards, onDeleteCard, onLikeCard, onDeleteCardsInBoard, board
       );
     });
   }
-  
-	const handleDeleteCards = async () => {
-  try {
-    await onDeleteCardsInBoard(boardId);
-    console.log(`All cards for board ${boardId} deleted`);
-  } catch (e) {
-    console.error(e);
-  }
-};
 
     return (
     <div className="card-list">
       {getCardListJSX(cards)}
-		<button 
-      className='reset-button' 
-      onClick={handleDeleteCards}
-      disabled={cards.length === 0}
-      >
-        Remove all cards
-      </button>
     </div>
   );
 };
@@ -51,8 +35,6 @@ CardList.propTypes = {
   ).isRequired,
   onDeleteCard: PropTypes.func.isRequired,
   onLikeCard: PropTypes.func.isRequired,
-	onDeleteCardsInBoard: PropTypes.func.isRequired,
-  boardId: PropTypes.number.isRequired,
 };
 
 export default CardList;
